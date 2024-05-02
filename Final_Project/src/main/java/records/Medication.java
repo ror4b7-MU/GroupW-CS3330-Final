68e89195f12231131497061cbdea356597c6b03f
Rachel Reiter <ror4b7@gmail.com>
2024-05-02 15:05:40 -0500

    Medication enum

--- Final_Project/src/main/java/records/Medication.java
+++ Final_Project/src/main/java/records/Medication.java
@@ -1,5 +1,36 @@
 package records;
 
 public enum Medication {
+	
+	//Lung medication 
+	ALBUTEROL, 
+	
+	//Pain medication 
+	AMOXICILLIN, 
+	
+	//Cancer medication 
+	ANASTROZOLE, 
+	
+	//Antibacterial 
+	BACTRIM, 
+	
+	//Heart failure medication 
+	DIGOXIN, 
+	
+	//Hypertension medication 
+	DILTIAZEM, 
+	
+	//Pain Medication 
+	IBUPROFEN,
+
+	//Cancer medication 
+	LETROZOLE, 
+	
+	//Antibacterial 
+	MACROBID, 
+	
+	//Chest pain medication 
+	VERAPAMIL, 
+
 
 }
