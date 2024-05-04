13f8dfd5d13476f5fd7cec5e5d6eed220436ef1c
Rachel Reiter <ror4b7@gmail.com>
2024-05-04 16:47:26 -0500

    Prescription edit to match syntax used in other methods

--- Final_Project/src/main/java/records/Prescription.java
+++ Final_Project/src/main/java/records/Prescription.java
@@ -1,5 +1,36 @@
 package records;
 
-public class Prescription {
+public enum Prescription {
+	
+	//Lung medication 
+	ALBUTEROL, 
+	
+	//Pain medication 
+	AMOXICILLIN, 
+	
+	//Cancer medication 
+	ANASTROZOLE, 
+	
+	//Antibacterial 
+	BACTRIM, 
+	
+	//Heart failure medication 
+	DIGOXIN, 
+	
+	//Hypertension medication 
+	DILTIAZEM, 
+	
+	//Pain Medication 
+	IBUPROFEN,
+
+	//Cancer medication 
+	LETROZOLE, 
+	
+	//Antibacterial 
+	MACROBID, 
+	
+	//Chest pain medication 
+	VERAPAMIL, 
+
 
 }
