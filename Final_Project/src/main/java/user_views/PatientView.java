e15115c084eff3dfa1d6887f6afa9db4b9491cf1
Athlie54 <kjself04@gmail.com>
2024-05-04 21:51:20 -0500

    added changeDoctor, referPatient

--- Final_Project/src/main/java/user_views/PatientView.java
+++ Final_Project/src/main/java/user_views/PatientView.java
@@ -1,6 +1,35 @@
 package user_views;
 
+import office_manager.OfficeManager;
+import users.Doctor;
+import users.User;
+
+import java.util.ArrayList;
+import java.util.Random;
+
 public class PatientView extends UserView {
 
+	private User user;
+	protected OfficeManager manager;
 	
+	public PatientView() {
+		//need constructor, but need userview constructor first
+	}
+	
+	Doctor changeDoctor(Doctor docName) {
+		ArrayList<Doctor> docList = manager.getDoctors(); //need getters and setters for the doctor and patient lists
+		ArrayList<Doctor> options = new ArrayList<>();
+		int length;
+		for (Doctor doc : docList) {
+			if (doc.getDoctorSpecialization()==docName.getDoctorSpecialization()) {
+				options.add(doc);
+				length+=1;
+			}
+		}
+		Random rand = new Random();
+		int randint = rand.nextInt(length);
+
+		return options.get(length);
+
+	}
 }
