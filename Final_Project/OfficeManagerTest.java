6e471c3c3570deb9ee21c64ae5b9f83ffe8fd3f2
Athlie54 <kjself04@gmail.com>
2024-05-05 13:09:17 -0500

    adjusted classpath

---
+++ Final_Project/OfficeManagerTest.java
@@ -0,0 +1,79 @@
+package Final_Project;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class OfficeManagerTest {
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+	}
+
+	@Test
+	void testGetInstance() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetPatientByUserName() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testAddDoctor() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testReturnSpecializedDoc() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testReturnGCP() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testReturnPodiatrist() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testReturnCardiologist() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testReturnOncologist() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testChangeDoctor() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetAppointments() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetPatients() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetDoctors() {
+		fail("Not yet implemented");
+	}
+
+}
