98136db247c271d91ac92dc21073af3e45263be9
Justice Mitchell <justice.s.mitchell@gmail.com>
2024-05-05 13:37:52 -0500

    adds all of justices to master, also comment out some of the other code in main

--- Final_Project/src/main/java/Main.java
+++ Final_Project/src/main/java/Main.java
@@ -1,6 +1,7 @@
 
 
 import java.util.Date;
+import java.util.Scanner;
 
 import javax.print.Doc;
 
@@ -11,33 +12,34 @@ import users.Doctor;
 import users.PatientUser;
 import users.Specialization;
 import users.User;
+import user_views.*;
 
 public class Main 
 {
 	public static void main(String []args)
     {
-		
-		
-		OfficeManager manager = OfficeManager.getInstance();
-		
-        Doctor doc1 = new Doctor("Bob", "Joel", "10/10", Specialization.CARDIOLOGIST); 
-        Doctor doc2 = new Doctor("Layla", "Smith", "10/10", Specialization.CARDIOLOGIST);
-/*
-* //adds doctors to singleton doc list 
-* 		if(!(manager.addDoctor(doc))) {
-* 			System.out.println("Failed to add"); }; if(!(manager.addDoctor(doc2))) {
-* 			System.out.println("Failed to add"); 
-* 		}
-*/
-        PatientUser pat1 = new PatientUser("Smith", "Name", "10/10", "n.smith"); 
-
-        PatientUser pat2 = new PatientUser("Bob", "Sur", "10/10", "s.bob"); 
-
-        Date start = new Date(2024, 3, 2, 3, 10); 
-        Date end = new Date(2024, 3, 2, 3, 30); 
-
-        Date start2 = new Date(2024, 3, 2, 3, 15); 
-        Date end2 = new Date(2024, 3, 2, 3, 40); 
+//		
+//		
+//		OfficeManager manager = OfficeManager.getInstance();
+//		
+//        Doctor doc1 = new Doctor("Bob", "Joel", "10/10", Specialization.CARDIOLOGIST); 
+//        Doctor doc2 = new Doctor("Layla", "Smith", "10/10", Specialization.CARDIOLOGIST);
+///*
+//* //adds doctors to singleton doc list 
+//* 		if(!(manager.addDoctor(doc))) {
+//* 			System.out.println("Failed to add"); }; if(!(manager.addDoctor(doc2))) {
+//* 			System.out.println("Failed to add"); 
+//* 		}
+//*/
+//        PatientUser pat1 = new PatientUser("Smith", "Name", "10/10", "n.smith"); 
+//
+//        PatientUser pat2 = new PatientUser("Bob", "Sur", "10/10", "s.bob"); 
+//
+//        Date start = new Date(2024, 3, 2, 3, 10); 
+//        Date end = new Date(2024, 3, 2, 3, 30); 
+//
+//        Date start2 = new Date(2024, 3, 2, 3, 15); 
+//        Date end2 = new Date(2024, 3, 2, 3, 40); 
 
 /****Rachel method tests****
 
@@ -107,6 +109,12 @@ public class Main
         //Should return True
         System.out.println(doc2.scheduleAppointment(pat2, start2, end2));*/
 
+		//Setup
+		// I think the system should be setup with a few doctors 
+		// Should probably have them setup initially here in main
+		// could have some placeholder Patients and appointments as well
+		// need to instantiate the OfficeManager here
+
         // Main program will start here by using scanner to ask the user if they are a Doctor or Patient
         // Based on their response a new instance of PatientView or DoctorView will be initialized
         // Then the UserView.Login method will be called wich will ask for the users name or ID 
@@ -117,7 +125,39 @@ public class Main
         // Might have an enum to represent the different options, or could just number them.
         // Could have a switch statement here in main or in the UserView to that would call the appropriate function
         // based on the user response
+		
+		// Im thinking this could maybe run in a loop that always loops back to the displayOptions part, 
+		// one of the options could be to logout. 
+		
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("Indicate (D) for doctor login or (P) for patient: ");
+		String DoctorOrPatient = scanner.nextLine(); 
+		UserView newUserView = null;
+		if(DoctorOrPatient.toLowerCase().equals("d")) {
+			newUserView = new DoctorView();
+		}
+		else if (DoctorOrPatient.toLowerCase().equals("p")) {
+			newUserView = new PatientView();
+		}
+		
+//		PatientView newUserView = new PatientView();
+		
+		// attempt to login
+		if(newUserView.login()) {
+			
+			// this loop will run until the user decides to logout
+			while(newUserView.getLoggedInStatus() == true) {
+				newUserView.runUserEnvironment();
+			}
+			System.out.println("Successfully Logged Out! Run the program again to log back in");
+		}
+		else {
+			System.out.println("Failed to login");
+		}
+		
 
+		
+		scanner.close();
     
 
     }
--- Final_Project/src/main/java/user_views/UserView.java
+++ Final_Project/src/main/java/user_views/UserView.java
@@ -6,43 +6,163 @@ import users.User;
 public abstract class UserView {
 	
 	// user object will be assigned by login() I'm thinking?
-	private User user;
-
+	protected User user; // this could also be userName not sure which would be easier
+	protected boolean loggedIn = false; // logged in status. Set to true on successful login. Set to false when the user logs out
+	protected Scanner scanner;
+	
+	public UserView() {
+		scanner = new Scanner(System.in);
+	}
+	
 	//Provide Access to the OfficeManager which will have all the office info
 	// This provides a clear way to get the singleton instance of OfficeManager
     protected OfficeManager getOfficeManager() {
         return OfficeManager.getInstance();
     }
-    
-    // function imitates logging into the system
-    // does so by simply setting the user in user view
-    public boolean login() {
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter username: ");
-        String userName = scanner.nextLine();
-
-        OfficeManager officeManager = getOfficeManager();
-        User user = officeManager.getPatientByUserName(userName);
-        if (user != null) {
-        	this.setUser(user);
-        	scanner.close();
-            return true;
-        } else {
-            System.out.println("Login failed.");
-            scanner.close();
-            return false;
-        }
-    }
-
-
-    //getter and setter
+	
+    //getters and setters
 	public User getUser() {
 		return user;
 	}
-
-
-	public void setUser(User user) {
+	
+	private void setUser(User user) {
 		this.user = user;
 	}
+	
+	public boolean getLoggedInStatus() {
+		return loggedIn;
+	}
+	
+	private void setLoggedInStatus(boolean loggedInStatus) {
+		this.loggedIn = loggedInStatus;
+	}
+
+    
+    // function imitates logging into the system
+    // does so by simply setting the user in user view
+	public boolean login() {
+	    //Scanner scanner = new Scanner(System.in);
+	    try {
+	        boolean loggedIn = false;
+	        while (!loggedIn) {
+	            System.out.print("Enter username to login: ");
+	            String userName = scanner.nextLine();
+	            
+	            OfficeManager officeManager = getOfficeManager();
+	            User user = officeManager.getUserByUserName(userName);
+	            
+	            if (user != null) {
+	                this.setUser(user);
+	                this.setLoggedInStatus(true);
+	                loggedIn = true;
+	            } else {
+	                System.out.println("User not found!");
+	                if (this instanceof PatientView) {
+	                    System.out.print("Would you like to create a new user? (Yes/No): ");
+	                    String yesOrNo = scanner.nextLine();
+	                    if (yesOrNo.equalsIgnoreCase("yes")) {
+	                        if (createNewUser()) {
+	                            // User created successfully, continue with login process
+	                            continue;
+	                        }
+	                    }
+	                }
+	                // If no user found and no new user is created, exit the loop
+	                break;
+	            }
+	        }
+	        return loggedIn;
+	    } catch (Exception e) {
+	        System.out.println("An error occurred: " + e.getMessage());
+	        return false;
+	    }
+	}
+    
+    
+    // if a userName is not found during login a new "account" (basically just a new User) can be created
+    // the subclasses will have to implement this method
+    // im thinking that in the DoctorView class it will just return false 
+    // because we dont just want to be able to create new doctors willy nilly?
+    // in the PatientView this method will scan for some user input and then call the the makeAccount method
+    // to actually create the new user and save it to the system
+    protected abstract boolean createNewUser();
+    
+    // maybe this isn't the greatest name but the point of the function is to provide an entry
+    // into the systems functionality by calling the two following functions displayOptions and executeSelectedOption
+    // this is the function that i want to run in a loop in main
+    public boolean runUserEnvironment() {
+    	// Scanner scanner = null;
+    	try { 
+    		if(this.loggedIn == false) { // throw an exception if no one is logged in
+    			throw new NotLoggedInException("Error: No user is logged in");
+    		}
+    		scanner = new Scanner(System.in);
+    		this.displayOptions(); // first display the options
+    		System.out.print("Please select an option by typing in a number: ");
+    		int optionNumber = scanner.nextInt(); // scan for the users input
+    		if (this.executeSelectedOption(optionNumber)) { // execute the selected option
+    			return true;
+    		}
+    		else {
+    			return false;
+    		}
+    	} catch (Exception e) { // if an exception is caught print it and return false
+			e.printStackTrace();
+			return false;
+		}
+	}
+    
+    // this function will display some options that the user can choose to perform
+    // see the subclasses for specifics
+    protected abstract void displayOptions();
+    
+    // has a switch statement to call the function related to that option
+    // again im making this abstract because there are slightly different options for Doctors and Patients
+    protected abstract boolean executeSelectedOption(int OptionNumber);
+    
+    // the following are the functions that will be executed based on a users selected option
+    // mostly they will just be wrappers for the officeManager functions that the rest of the team is writing
+    // and they scan for user input so that the officeManager functions can have parameters\
+    
+    protected void viewSchedule() {
+    	
+    }
+    
+    protected boolean bookAppointment() {
+    	return false;
+    }
+    
+    protected boolean cancelOrReschedulApp() {
+		return false;
+    	
+    }
+    
+    // logs the user out
+    // its nothing fancy just sets the user to null and the loggedIn status to false
+    protected boolean logout() {
+    	try {
+    		if(this.loggedIn == false) {
+    			throw new NotLoggedInException("Error: No user is logged in");
+    		}
+    		this.setUser(null);
+    		this.setLoggedInStatus(false);
+    		return true;
+    	}
+    	catch (Exception e){
+    		e.printStackTrace();
+    		return false;
+    	}	
+    }
+    
+   
+    // not logged in exception
+    protected class NotLoggedInException extends Exception {
+		private static final long serialVersionUID = 1L;
+
+		public NotLoggedInException(String message) {
+            super(message);
+        }
+    }
+
 	
 }
